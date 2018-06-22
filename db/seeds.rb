fa574899e45e4fb8ac6d8e0653d2a91e51d0b5fd
Alex Kibler <alexkibler@me.com>
2018-06-21 18:16:43 -0600

    fill out seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+require 'faker'
+
+10.times do
+  Blog.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph
+  )
+end
+
+puts '10 blog posts created'
+
+5.times do
+  Skill.create!(
+    title: Faker::Lorem.sentence,
+    percent_utilized: Faker::Number.number(2)
+  )
+end
+
+puts '5 skills created'
+
+9.times do
+  Portfolio.create!(
+    title: Faker::Lorem.sentence,
+    subtite: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph,
+    main_image: Faker::LoremPixel.image,
+    thumb_image: Faker::LoremPixel.image
+  )
+end
+
+puts '9 porfolio items created'
